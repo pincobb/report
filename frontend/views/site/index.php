e3d8921c567270a9f44c85aaaff0473a76c98792
NOTE <NOTE@192.168.32.1>
2018-03-21 16:18:23 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,24 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'ระบบรายงาน';
 ?>
-<center>ทดสอบระบบ</center>
+<?php
+$route1= Yii::$app->urlManager->createUrl('test/test1');
+$route2= Yii::$app->urlManager->createUrl(['test/test2','name'=>'kitsada','lname'=>'tidwilas']);
+?>
+<a href="<?=$route1?>">ไปที่ test1</a><br>
+<a href="<?=$route2?>">ไปที่ test2</a>
+<br>
+<?=
+Html::a('ไปที่ test3',['test/test1','a'=>'1']);
+?>
+
+<?=
+yii\helpers\Url::to('test/test1','aaa')
+?>
+<form action="" method="get">
+    ชื่อ<input
+
+</form>
